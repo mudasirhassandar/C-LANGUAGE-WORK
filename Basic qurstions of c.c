ba67713afd13220b1c37eab74b2c6882cd3cd31c
mudasirhassandar <mudasirhassan391@gmail.com>
2025-06-25 12:27:32 +0530

    Add files via upload

---
+++ Basic qurstions of c.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+ int main(){
+
+printf("hello mudasir \n");
+printf("hello mudasir \n");
+printf("hello mudasir \n");
+printf("hello mudasir \n");
+
+return 0;
+}
+
+int main (){
+int age=20;
+float pi=3.14;
+char star='*';
+
+printf("age is %d \n",age );
+printf("value of pi %f \n",pi );
+printf("star looks like this %c \n",star);
+
+return 0;
+}
+
+int main(){
+
+    int age;
+printf("enter age");
+scanf("%d",&age);
+printf("age is: %d",age);
+return 0;
+}
+
+int main(){
+
+float a,b;
+
+printf("enter a");
+scanf("%f", &a);
+
+printf("enter b");
+scanf("%f", &b);
+
+float sum= a+b;
+
+printf("sum is %f", sum);
+
+return 0;
+
+}
+
+
+//write a program to calculate area of circle;
+
+int main(){
+float radius;
+ printf("enter radius\n");
+ scanf("%f\n", &radius);
+
+printf("area of cricle is:%f", 3.14*radius*radius);
+return 0;
+}
+
