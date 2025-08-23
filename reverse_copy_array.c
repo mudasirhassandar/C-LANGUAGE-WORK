71a5b355fd31434866f99d7fa2c20c7e1827a16e
mudasirhassandar <mudasirhassan391@gmail.com>
2025-08-23 11:41:14 +0530

    Update reverse_copy_array.c

--- reverse_copy_array.c
+++ reverse_copy_array.c
@@ -35,3 +35,52 @@ int main()
 
     return 0;
 }
+// METHOD 2;
+// WAP to pint reverse of array without using another array?
+
+#include <stdio.h>
+void fun(int x, int arr[])
+{
+    int i = 0;
+    int j = x - 1;
+    while (i < j)
+    {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+
+    return;
+}
+int main()
+{
+    int x;
+    printf("Enter size of your array  : ");
+    scanf("%d", &x);
+    int arr[x];
+    int brr[x];
+
+    for (int i = 0; i < x; i++)
+    {
+        printf("Enter element number %d\n", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Your entered array = ");
+    for (int i = 0; i < x; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+    printf("Your Reversed Array = ");
+    fun(x, arr);
+    for (int i = 0; i < x; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
